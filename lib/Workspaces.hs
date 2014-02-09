ba2f0bbd650e79a9b7b68f0233d4228d2b4b7e82
Magnus Müller <mamuelle@informatik.hu-berlin.de>
2014-02-09 14:56:37 +0100

    rename workspaces

--- lib/Workspaces.hs
+++ lib/Workspaces.hs
@@ -4,6 +4,6 @@ where
 import XMonad
 
 myWorkSpaces :: [WorkspaceId]
-myWorkSpaces = ["1:im", "2:www", "3:mail", "4:IRC", "5:dev", "6:ddd", "7:write"] ++ map show [8..8] ++ ["9:gimp"]
+myWorkSpaces = ["1:im", "2:www", "3:vsn", "4:dev0", "5:dev1", "6:dev2", "7:dev3"] ++ map show [8..8] ++ ["9:gimp"]
 
 
